06607d5f81c5ff50c796c6a7cedc297d98d4ed56
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-16 14:16:49 -0500

    all numbers divisible by 3 are changed to mined

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,25 +1,9 @@
-#100.times do |number|
-  #  number += 1
- #   puts number 
-
-#	if number == 3
-#	puts 'mined'
-#	number += 1
-#	else if number == 5
-#		puts 'minds'
-#		number += 1
-#	else if number == 15
-#		puts 'mined minds'
-#		number += 1
-#	else
-#	puts number
-	#end
 
 number = 1
 
 100.times do
 
-	if number == 3
+	if number %3 == 0
 			puts "mined"
 	elsif number ==5
 			puts "minds"
