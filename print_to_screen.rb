ebf807851b9963ed4bc7845e41db20cd9d9e6ff1
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-16 14:00:44 -0500

    print mined instead of the number 3

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,4 +1,29 @@
-100.times do |number|
-    number += 1
-    puts number
-end 
+#100.times do |number|
+  #  number += 1
+ #   puts number 
+
+#	if number == 3
+#	puts 'mined'
+#	number += 1
+#	else if condition if number == 5
+#		puts 'minds'
+#		number += 1
+#	else if if number == 15
+#		puts 'mined minds'
+#		number += 1
+#	else
+#	puts number
+	#end
+
+	number = 1
+
+	100.times do
+
+		if number == 3
+			puts "mined"
+			number = number + 1
+		else
+			puts number
+			number = number + 1
+		end
+end
